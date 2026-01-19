fad15cd566e7e6676c7e006c3730ed42a6fcf3c7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-19 08:35:35 +0000

    .

--- .github/docObjModel/js/script.js
+++ .github/docObjModel/js/script.js
@@ -1,3 +1,5 @@
 //Old version of working with HTML elements
-const box = document.getElementById('box')
+//const box = document.getElementById('box')
+
+const box = document.getElementsByClassName('box') //always returns an array of HTML Collection
 console.log(box)
