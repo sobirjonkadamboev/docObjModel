2b829eb319bfb1d88aadd567d5f8ca7a922b41b8
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-20 14:34:21 +0000

    .

--- 18-dars. Amaliyot. DOM/js/script.js
+++ 18-dars. Amaliyot. DOM/js/script.js
@@ -1,8 +1,22 @@
 const blockAd = document.querySelectorAll('.promo__adv img'),
-	genreChange = document.querySelector('.promo__genre')
+	genreChange = document.querySelector('.promo__genre'),
+	background = document.querySelector('.promo__bg'),
+	seris = document.querySelectorAll('promo__interactive-list')
+
+const seriesDB = {
+	series: [
+		'Omar',
+		'The Final Legacy',
+		'Ertugrul',
+		'Magneficent Century',
+		'The Great Seljuks: Guardians...',
+	],
+}
 
 blockAd.forEach(item => {
 	item.remove()
 })
 
 genreChange.textContent = 'Comedy'
+
+background.style.backgroundImage = 'url(./img/1.jpg)' //move from index.html not this .js
