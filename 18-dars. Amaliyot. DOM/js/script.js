7040580833ac2d4985c79d95c4e5556e545ca5db
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-20 14:18:49 +0000

    .

--- 18-dars. Amaliyot. DOM/js/script.js
+++ 18-dars. Amaliyot. DOM/js/script.js
@@ -1,33 +1,8 @@
-const advs = document.querySelectorAll('.promo__adv img'),
-	genre = document.querySelector('.promo__genre'),
-	wrapper = document.querySelector('.promo__bg'),
-	seriesList = document.querySelector('.promo__interactive-list')
+const blockAd = document.querySelectorAll('.promo__adv img'),
+	genreChange = document.querySelector('.promo__genre')
 
-const seriesDB = {
-	series: [
-		'Omar',
-		'The Final Legacy',
-		'Ertugrul',
-		'Magnificent Century',
-		'The Great Seljuks: Guardians...',
-	],
-}
-
-advs.forEach(item => {
+blockAd.forEach(item => {
 	item.remove()
 })
 
-genre.textContent = 'Comedy'
-
-wrapper.style.backgroundImage = 'url(./img/1.jpg)'
-
-seriesList.innerHTML = ''
-
-seriesDB.series.forEach((item, index) => {
-	seriesList.innerHTML += `
-  <li class="promo__interactive-item">
-    ${index + 1}. ${item}
-    <div class="delete"></div>
-  </li>
-  `
-})
+genreChange.inn
