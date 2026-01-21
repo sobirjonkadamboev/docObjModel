e850f3b730cc8bfc4551ad897385dd7544f411b8
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-21 07:52:14 +0000

    d

--- .github/events/script.js
+++ .github/events/script.js
@@ -1,5 +1,18 @@
 const btn = document.querySelector('button')
 
-btn.onclick = function () {
-	alert(1)
+/* btn.onclick = function () {
+	console.log(1)
 }
+
+btn.ondblclick = function () {
+	console.log(2)
+}
+*/
+
+btn.addEventListener('click', () => {
+	console.log('first click')
+})
+
+btn.addEventListener('click', () => {
+	console.log('second  click')
+})
