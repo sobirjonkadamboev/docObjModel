efb22e155508b8ae6449998366c8fd8ddc2d0838
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-23 17:00:32 +0000

    .

--- .github/events/script.js
+++ .github/events/script.js
@@ -1,22 +1,37 @@
-const btn = document.querySelector('button')
+const link = document.querySelector('.a_link')
 
-btn.addEventListener('click', event => {
+link.addEventListener('click', event => {
+	event.preventDefault()
+	console.log('click')
+})
+
+/* const btn = document.querySelector('button'),
+	overlay = document.querySelector('.overlay')
+
+ const callback = event => {
+	console.log(event.currentTarget)
+}
+
+overlay.addEventListener('click', callback)
+btn.addEventListener('click', callback)
+*/
+/* btn.addEventListener('click', event => {
 	console.log(event.target)
 	event.target.style.backgroundColor = 'yellow'
 	event.target.textContent = 'Button'
 })
 
 let counter = 0
-
-const deleteElement = event => {
+*/
+/* const deleteElement = event => {
 	counter++
 	console.log(counter)
 	if (counter == 4) {
 		btn.removeEventListener('click', deleteElement)
 	}
 }
-
-btn.addEventListener('click', deleteElement)
+*/
+//btn.addEventListener('click', deleteElement)
 
 //event.target.remove() - for removing events going on variables
 /* btn.onclick = function () {
