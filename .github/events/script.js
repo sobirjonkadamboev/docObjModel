28d7d0384875ba93e5470dc005e7147c71f42e65
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-23 09:48:13 +0000

    ..#

--- .github/events/script.js
+++ .github/events/script.js
@@ -4,7 +4,6 @@ btn.addEventListener('click', event => {
 	console.log(event.target)
 	event.target.style.backgroundColor = 'yellow'
 	event.target.textContent = 'Button'
-	console.log('first click')
 })
 
 /* btn.onclick = function () {
