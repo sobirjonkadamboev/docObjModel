b99efbdd3299985eb9a3698b2f6714f5fb37fbb3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-25 09:10:20 +0000

    .

--- .github/practice.js
+++ .github/practice.js
@@ -5,7 +5,10 @@ let a = 4
 let b = a
 //console.log(a + b) //for declaring variables
 
-let x, y, z
+let x, y, z //declaring 3 variables at once
 x = 19
 y = 20
 z = 18
+
+let _lastName = 'Sobirjon'
+console.log(_lastName)
