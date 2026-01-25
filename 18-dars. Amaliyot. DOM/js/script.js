a12e01f200ee58d5b3c81803180adf45834cfa3b
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-25 05:49:33 +0000

    .

--- 18-dars. Amaliyot. DOM/js/script.js
+++ 18-dars. Amaliyot. DOM/js/script.js
@@ -1,29 +1,33 @@
-const blockAd = document.querySelectorAll('.promo__adv img'),
-	genreChange = document.querySelector('.promo__genre'),
-	background = document.querySelector('.promo__bg'),
-	seriesList = document.querySelector('.promo__interactive-list')
+document.addEventListener('DOMContentLoaded', function () {
+	const blockAd = document.querySelectorAll('.promo__adv img'),
+		genreChange = document.querySelector('.promo__genre'),
+		background = document.querySelector('.promo__bg'),
+		seriesList = document.querySelector('.promo__interactive-list'),
+		formBN = document.querySelector('form'),
+		submitBTN = document.querySelector('button')
 
-const seriesDB = {
-	series: [
-		'Omar',
-		'The Final Legacy',
-		'Ertugrul',
-		'Magneficent Century',
-		'The Great Seljuks: Guardians...',
-	],
-}
+	const seriesDB = {
+		series: [
+			'Omar',
+			'The Final Legacy',
+			'Ertugrul',
+			'Magneficent Century',
+			'The Great Seljuks: Guardians...',
+		],
+	}
 
-blockAd.forEach(item => {
-	item.remove()
-})
+	blockAd.forEach(item => {
+		item.remove()
+	})
 
-genreChange.textContent = 'Comedy'
+	genreChange.textContent = 'Comedy'
 
-background.style.backgroundImage = 'url(./img/1.jpg)' //move from index.html not this .js
+	background.style.backgroundImage = 'url(./img/1.jpg)' //move from index.html not this .js
 
-seriesList.innerHTML = ''
+	seriesList.innerHTML = ''
 
-seriesDB.series.forEach((item, index) => {
-	seriesList.innerHTML += `<li class="promo__interactive-item"> ${index + 1} ${item}
+	seriesDB.series.forEach((item, index) => {
+		seriesList.innerHTML += `<li class="promo__interactive-item"> ${index + 1} ${item}
 	<div class="delete"></div></li>`
+	})
 })
