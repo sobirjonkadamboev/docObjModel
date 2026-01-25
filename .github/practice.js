a3a63cbd943199f96793b4fb25f86dd101c63fe0
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-01-25 08:54:07 +0000

    m

--- .github/practice.js
+++ .github/practice.js
@@ -1,1 +1,11 @@
-console.log('Hi, buddies')
+document.getElementById('paragraph').innerText = 'Text'
+//window.alert(5)
+// this is a comment
+let a = 4
+let b = a
+//console.log(a + b) //for declaring variables
+
+let x, y, z
+x = 19
+y = 20
+z = 18
