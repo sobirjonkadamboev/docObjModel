9e1d9bd7981c67d05f56919e2ab594750183d5a7
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-02 16:41:53 +0000

    finish today

--- 18-dars. Amaliyot. DOM/app.js
+++ 18-dars. Amaliyot. DOM/app.js
@@ -3,3 +3,7 @@
 let myName = 'Sobirjon'
 let myCity = 'Seoul'
 console.log(`My name is ${myName}. My favorite city is ${myCity}`)
+
+//typeof operator
+let newVariable = 'Playing around with typeof.'
+console.log(typeof newVariable)
