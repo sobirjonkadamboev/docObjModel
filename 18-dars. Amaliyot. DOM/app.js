8f6069cad27380bf9496c1fc5daf3bd486753261
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-05 10:21:05 +0000

    .

--- 18-dars. Amaliyot. DOM/app.js
+++ 18-dars. Amaliyot. DOM/app.js
@@ -1,4 +1,4 @@
-//string interpolation
+/* string interpolation
 
 let myName = 'Sobirjon'
 let myCity = 'Seoul'
@@ -7,3 +7,16 @@ console.log(`My name is ${myName}. My favorite city is ${myCity}`)
 //typeof operator
 let newVariable = 'Playing around with typeof.'
 console.log(typeof newVariable)
+ */
+
+//mini project by codecademy
+//forecast today is 293 kelvin
+
+//forecast today is 293 kelvin
+const kelvin = 293
+//subtracted to find celcius
+const celsius = kelvin - 273
+//finding fahrenheit
+//using math.floor
+let fahrenheit = Math.floor(celsius * (9 / 5) + 32)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
