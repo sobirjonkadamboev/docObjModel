cd6cc3b166d47ede925639d4064a40fd7d52af62
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-05 10:25:56 +0000

    .

--- 18-dars. Amaliyot. DOM/app.js
+++ 18-dars. Amaliyot. DOM/app.js
@@ -19,3 +19,5 @@ const celsius = kelvin - 273
 //using math.floor
 let fahrenheit = Math.floor(celsius * (9 / 5) + 32)
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+//second project by codecademy
